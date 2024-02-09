36b5c36fac3c2aa70c3e6f7c98376a7e8ed7005f
ozioma45 <oziomaegole@gmail.com>
2024-02-09 13:23:21 +0100

    allow user input

--- Main/cardConsoleValidation.js
+++ Main/cardConsoleValidation.js
@@ -1,3 +1,21 @@
-const cardValidate = /^4[0-9]{12}(?:[0-9]{3})?$/.test("422222222222");
+/* const cardValidate = /^4[0-9]{12}(?:[0-9]{3})?$/.test("422222222222");
 
-console.log(cardValidate);
+console.log(cardValidate); */
+
+//accepting input from user
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter your credit card number: ", (userInput) => {
+  // Regular expression to validate credit card number
+  const cardValidate = /^4[0-9]{12}(?:[0-9]{3})?$/.test(userInput);
+
+  // Display the result
+  console.log(cardValidate);
+
+  rl.close();
+});
